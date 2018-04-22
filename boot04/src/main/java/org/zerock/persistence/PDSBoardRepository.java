1a944c839d628c4c38e37305d313eb75f9198231
astinchoi <asciineo@gmail.com>
2018-04-22 22:16:23 +0900

    finish ch4

---
+++ boot04/src/main/java/org/zerock/persistence/PDSBoardRepository.java
@@ -0,0 +1,28 @@
+package org.zerock.persistence;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.PDSBoard;
+
+import java.util.List;
+
+public interface PDSBoardRepository extends CrudRepository<PDSBoard, Long> {
+
+    @Modifying
+    @Query("UPDATE FROM PDSFile f " +
+           "SET f.pdsfile = ?2 " +
+           "WHERE f.fno = ?1")
+    public int updatePDSFile(Long fno, String newFileName);
+
+    @Modifying
+    @Query("DELETE FROM PDSFile f WHERE f.fno = ?1")
+    public int deletePDSFile(Long fno);
+
+    @Query("SELECT p, count(f) " +
+           "FROM PDSBoard p LEFT OUTER JOIN p.files f ON p.id = f " +
+           "WHERE p.pid > 0 " +
+           "GROUP BY p " +
+           "ORDER BY p.pid DESC")
+    public List<Object[]> getSummary();
+}
